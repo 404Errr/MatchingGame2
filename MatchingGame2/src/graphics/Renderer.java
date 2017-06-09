6d63e694f242c5926a50fcfd25f789b84762f80e
aaschmei.17 <aaschmei.17@CHS-CRT9-12.CHUFSD.Org>
2017-06-09 10:24:28 -0400

    comment removed

--- MatchingGame2/src/graphics/Renderer.java
+++ MatchingGame2/src/graphics/Renderer.java
@@ -46,36 +46,6 @@ public class Renderer extends JPanel implements Data {
 		}
 	}
 	
-//	private void drawScore() {
-//		g.setFont(new Font("Helvetica", Font.BOLD, UI_PADDING));
-//		g.setColor(Color.BLACK);
-//		g.drawString("SCORE: "+Game.getPlayerScore(), UI_PADDING, UI_PADDING*2);
-//	}
-//
-//	private void drawChoiceButtons() {
-//		ArrayList<Button> buttons = Buttons.getButtons();
-//		g.setFont(new Font("Helvetica", Font.BOLD, UI_BUTTON_HEIGHT/2));
-//		for (int i = 0;i<buttons.size();i++) {
-//			Button b = buttons.get(i);
-//			if (b.isHovered()) {
-//				g.setColor(COLOR_UI_BUTTON_HOVERED);
-//			}
-//			else {
-//				g.setColor(COLOR_UI_BUTTON);
-//			}
-//			g.fill3DRect(b.getBounds().x, b.getBounds().y, b.getBounds().width, b.getBounds().height, !b.isPressed());
-//			if (Game.getCurrentItemSet()!=null) {
-//				g.setColor(Color.BLACK);
-//				if (SHOW_CORRECT) {
-//					if (Game.getCurrentItemSet().getChoices().get(i)==Game.getCurrentItemSet().getCorrectItem()) {
-//						g.setColor(Color.ORANGE);
-//					}
-//				}
-//				g.drawString(Game.getCurrentItemSet().getChoices().get(i).getName(), b.getBounds().x+UI_BUTTON_HEIGHT/4, b.getBounds().y+UI_BUTTON_HEIGHT*11/16);
-//			}
-//		}
-//	}
-//
 	private void drawImage(Graphics2D g, BufferedImage image) {//draws image
 		g.drawImage(image, IMAGE_X, IMAGE_Y, IMAGE_SIZE, IMAGE_SIZE*(image.getHeight()/image.getWidth()), null);
 	}
